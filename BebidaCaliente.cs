fe7d5a62e6b1418687fd69c1765abc6bf2eeaddc
Juan Jose Martinez Guillen <20251014@uthh.edu.mx>
2026-05-25 20:53:29 -0600

    agregue bebidas

---
+++ BebidaCaliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class BebidaCaliente : bebida
+    {
+        private int Temperatura;
+
+        public int temperatura
+        {
+            get { return Temperatura; }
+            set { Temperatura = value; }
+        }
+
+        //constructor en cuando heredamos 
+        public BebidaCaliente(string nombre, string tamano, float precio, int _temperatura) : base(nombre, tamano, precio)
+        {
+            Temperatura = _temperatura;
+        }
+
+        //metodo sobreescrito(Polimorfismo)
+        public override string preparar()
+        {
+            return "Preparando un : " + nombre + " Caliente  de tamaño : " + tamano + " a una temperatura de : " + Temperatura + "°C";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,148 +1,63 @@
 ﻿namespace ConsoleApp35
 {
-    class Bebida
-    {
-
-        // Atributos privados
-
-        private string nombre;
-
-        private string tamaño;
-
-        private double precio;
-
-        // Constructor
-
-        public Bebida(string nombre, string tamaño, double precio)
-
-        {
-
-            this.nombre = nombre;
-
-            this.tamaño = tamaño;
-
-            this.precio = precio;
-
-        }
-
-        // Método que RETORNA el mensaje
-
-        public string Preparar()
-
-        {
-
-            return "Preparando un " + nombre + " tamaño " + tamaño + "...";
-
-        }
-
-        // Método para aplicar descuento
-
-        public void AplicarDescuento(double porcentaje)
-
-        {
-
-            double descuento = precio * (porcentaje / 100);
-
-            precio = precio - descuento;
-
-        }
-
-        // Método que RETORNA descripción
-
-        public string MostrarDescripcion()
-
-        {
-
-            return "Esto es un " + nombre +
-
-                   " de tamaño " + tamaño +
-
-                   " con un costo de $" + precio;
-
-        }
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
             string nombre, tamaño;
-
-            double precio;
-
-            // ===== BEBIDA 1 =====
+            float precio;
+            // Primera bebida
 
             Console.WriteLine("Ingrese el nombre de la bebida 1:");
-
             nombre = Console.ReadLine();
 
             Console.WriteLine("Ingrese el tamaño:");
-
             tamaño = Console.ReadLine();
 
             Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
 
-            precio = Convert.ToDouble(Console.ReadLine());
-
-            Bebida bebida1 = new Bebida(nombre, tamaño, precio);
-
-            // ===== BEBIDA 2 =====
-
+            bebida bebida1 = new bebida(nombre, tamaño, precio);
+            // Segunda bebida
             Console.WriteLine("\nIngrese el nombre de la bebida 2:");
-
             nombre = Console.ReadLine();
 
             Console.WriteLine("Ingrese el tamaño:");
-
             tamaño = Console.ReadLine();
 
             Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
 
-            precio = Convert.ToDouble(Console.ReadLine());
-
-            Bebida bebida2 = new Bebida(nombre, tamaño, precio);
-
-            // ===== BEBIDA 3 =====
-
+            bebida bebida2 = new bebida(nombre, tamaño, precio);
+            // Tercera bebida
             Console.WriteLine("\nIngrese el nombre de la bebida 3:");
-
             nombre = Console.ReadLine();
 
             Console.WriteLine("Ingrese el tamaño:");
-
             tamaño = Console.ReadLine();
 
             Console.WriteLine("Ingrese el precio:");
+            precio = float.Parse(Console.ReadLine());
 
-            precio = Convert.ToDouble(Console.ReadLine());
+            bebida bebida3 = new bebida(nombre, tamaño, precio);
+            bebida1.AplicarDescuento(20);
+            bebida2.AplicarDescuento(10);
+            bebida3.AplicarDescuento(0);
 
-            Bebida bebida3 = new Bebida(nombre, tamaño, precio);
+            Console.WriteLine("Preparando....");
+            Console.WriteLine("\n" + bebida1.preparar());
+            Console.WriteLine("\n" + bebida2.preparar());
+            Console.WriteLine("\n" + bebida3.preparar());
 
-            // Aplicar descuento
 
-            bebida1.AplicarDescuento(10);
-
-            // Mostrar preparación
-
-            Console.WriteLine("\n" + bebida1.Preparar());
-
-            Console.WriteLine(bebida2.Preparar());
-
-            Console.WriteLine(bebida3.Preparar());
-
-            // Mostrar descripción
-
-            Console.WriteLine("\n--- DESCRIPCIÓN DE BEBIDAS ---");
-
-            Console.WriteLine(bebida1.MostrarDescripcion());
-
-            Console.WriteLine(bebida2.MostrarDescripcion());
-
-            Console.WriteLine(bebida3.MostrarDescripcion());
+            Console.WriteLine("\n Descripcion del pedido....");
+            Console.WriteLine("\n" + bebida1.MostrarDescripcion());
+            Console.WriteLine("\n" + bebida2.MostrarDescripcion());
+            Console.WriteLine("\n" + bebida3.MostrarDescripcion());
 
             Console.ReadKey();
-
         }
     }
-    
+
 }
